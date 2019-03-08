7cd3a7e2535b0d6aa8557dabc18749f0e282470b
Antonio Vega <a.vegaramirez76@gmail.com>
2019-03-08 12:58:43 +0900

    First commit

---
+++ CoinsWorld/Assets/Scripts/GameManager.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState
+{
+    menu,
+    inTheGame,
+    gameOver,
+    wonTheGame
+}
+
+public class GameManager : MonoBehaviour
+{
+    // Actual GameState
+    [HideInInspector]
+    public GameState currentGameState;
+
+    // singleton
+    public static GameManager sharedInstance;
+
+    public Canvas menuCanvas;
+    public Canvas gameCanvas;
+    // public Canvas gameCanvasVR;
+    public Canvas gameOverCanvas;
+    public Canvas gameWonCanvas;
+
+    public bool theGameStart = false;
+
+    void Awake()
+    {
+        // Initialize the singleton and share all the GameManager fields and methods with it
+        sharedInstance = this;
+    }
+
+    void Start()
+    {
+        currentGameState = GameState.menu;
+
+        // Setup the canvas behaviour
+        menuCanvas.enabled = true;
+        gameCanvas.enabled = false;
+        // gameCanvasVR.enabled = false;
+        gameOverCanvas.enabled = false;
+        gameWonCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        // If the user want to quit the game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitTheGame();
+        }
+
+        if (currentGameState != GameState.inTheGame && currentGameState != GameState.wonTheGame && GlobalStaticVariables.theUserResetTheGame == false)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                StartGame();
+            }
+        }
+        else if (currentGameState != GameState.inTheGame && currentGameState != GameState.wonTheGame && GlobalStaticVariables.theUserResetTheGame == true)
+        {
+            StartGame();
+        }
+        else if (currentGameState == GameState.wonTheGame)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                // Reload the scene
+                GameSceneManager.sharedInstance.GameScene();
+            }
+            //else if (Input.GetKeyDown(KeyCode.Escape))
+            //{
+            //    QuitTheGame();
+            //}
+        }
+    }
+
+    // Use this for start the game
+    public void StartGame()
+    {
+
+        GlobalStaticVariables.theUserResetTheGame = false;
+        UICountDown.sharedInstance.StartTheCountDown();
+        UICountDown.sharedInstance.PlayerMovement();
+        theGameStart = true;
+        ChangeGameState(GameState.inTheGame);
+    }
+
+    // Called when the player dies
+    public void GameOver()
+    {
+        theGameStart = false;
+        ChangeGameState(GameState.gameOver);
+    }
+
+    // Called when the player dies
+    public void GameWon()
+    {
+        GlobalStaticVariables.theUserResetTheGame = true;
+        theGameStart = false;
+        ChangeGameState(GameState.wonTheGame);
+    }
+
+    void QuitTheGame()
+    {
+        Application.Quit();
+    }
+
+    // This method will manage the states of the game
+    void ChangeGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+            // The logic of the principal menu
+            menuCanvas.enabled = true;
+            gameCanvas.enabled = false;
+            // gameCanvasVR.enabled = false;
+            gameOverCanvas.enabled = false;
+            gameWonCanvas.enabled = false;
+        }
+        else if (newGameState == GameState.inTheGame)
+        {
+            // This is the current scene or level of the game
+            menuCanvas.enabled = false;
+            gameCanvas.enabled = true;
+            // gameCanvasVR.enabled = true;
+            gameOverCanvas.enabled = false;
+            gameWonCanvas.enabled = false;
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            // Gameover
+            menuCanvas.enabled = false;
+            gameCanvas.enabled = false;
+            // gameCanvasVR.enabled = false;
+            gameOverCanvas.enabled = true;
+            gameWonCanvas.enabled = false;
+        }
+        else if (newGameState == GameState.wonTheGame)
+        {
+            // Gamewon
+            menuCanvas.enabled = false;
+            gameCanvas.enabled = false;
+            // gameCanvasVR.enabled = false;
+            gameOverCanvas.enabled = false;
+            gameWonCanvas.enabled = true;
+        }
+
+        // This is the new state after the change 
+        currentGameState = newGameState;
+
+    }
+}
---
+++ CoinsWorld/Assets/Scripts/UICountDown.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class UICountDown : MonoBehaviour
+{
+    public static UICountDown sharedInstance;
+
+    public static float TimerBonus = 0;
+
+    public int GameTime = 60;
+
+    private Text _countDownText;
+
+    private GameObject player;
+    private FirstPersonController characterControllerScript;
+    private AudioSource playerAudio;
+
+    private float _countDownTimerDuration;
+    private float _countDownTimerStartTime;
+
+    string timerMessage;
+    int timeLeft;
+
+    [HideInInspector]
+    public bool theGameIsCounting = true;
+
+    void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _countDownText = GetComponent<Text>();
+
+        player = GameObject.FindGameObjectWithTag("PlayerFPS");
+        characterControllerScript = player.GetComponent<FirstPersonController>();
+        playerAudio = player.GetComponent<AudioSource>();
+
+        characterControllerScript.m_WalkSpeed = 0.0f;
+        characterControllerScript.m_RunSpeed = 0.0f;
+
+        // SetUpCountDownTimer(GameTime); // this is the time that will be provide to the user
+    }
+
+    // This is a probe
+    public void StartTheCountDown()
+    {
+        SetUpCountDownTimer(GameTime); // this is the time that will be provide to the user
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.sharedInstance.theGameStart == true && theGameIsCounting == true)
+        {
+            if (TimerBonus > 0)
+            {
+                _countDownTimerStartTime += TimerBonus;
+                TimerBonus = 0; // reset the variable or it will generate a bug
+            }
+
+            timeLeft = (int)CountDownTimeRemaning();
+            if (timeLeft > 0)
+            {
+                timerMessage = "Timer: " + LeadingZero(timeLeft);
+            }
+            else
+            {
+                timerMessage = "Game Over";
+                StopPlayerMovement();
+                GameManager.sharedInstance.GameOver();
+                Debug.Log("The game is finished"); 
+            }
+
+            _countDownText.text = timerMessage;
+        }
+    }
+
+    // This will setup the timing 
+    void SetUpCountDownTimer(float DelayInSeconds)
+    {
+        _countDownTimerDuration = DelayInSeconds;
+        _countDownTimerStartTime = Time.time;
+    }
+
+    private float CountDownTimeRemaning()
+    {
+        float _elapsedSeconds = Time.time - _countDownTimerStartTime;
+        float _timeLeft = _countDownTimerDuration - _elapsedSeconds;
+        return _timeLeft;
+    }
+
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(3, '0');
+    }
+
+    public void PlayerMovement()
+    {
+        characterControllerScript.m_WalkSpeed = 5.0f;
+        characterControllerScript.m_RunSpeed = 10.0f;
+    }
+
+    public void StopPlayerMovement()
+    {
+        characterControllerScript.m_WalkSpeed = 0.0f;
+        characterControllerScript.m_RunSpeed = 0.0f;
+    }
+
+}
