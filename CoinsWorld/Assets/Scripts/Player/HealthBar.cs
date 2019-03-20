a112b4f04335acc2bc63b2b276b98dca2cfde423
Antonio Vega <a.vegaramirez76@gmail.com>
2019-03-20 10:52:34 +0900

    Health bar
    Change the color of the health bar depending of the user life

--- CoinsWorld/Assets/Scripts/Player/HealthBar.cs
+++ CoinsWorld/Assets/Scripts/Player/HealthBar.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    private GameObject healthObjectBar;
     private RectTransform healthBar;
+    private Image imageColor;
     private Text healthText;
+
+    private Color colorGreen;
+    private Color colorYellow;
+    private Color colorRed;
 
     private float maxLife = 1;
     private float lifeReduce;
@@ -16,20 +22,42 @@ public class HealthBar : MonoBehaviour
 
     void Start()
     {
-        healthBar = GameObject.FindGameObjectWithTag("HealthBarAnimation").GetComponent<RectTransform>();
+        healthObjectBar = GameObject.FindGameObjectWithTag("HealthBarAnimation");
+        healthBar = healthObjectBar.GetComponent<RectTransform>();
+        imageColor = healthObjectBar.GetComponent<Image>();
         healthText = GameObject.FindGameObjectWithTag("LifeText").GetComponent<Text>();
         lifeReduce = maxLife;
+
+        colorGreen = new Color32(105, 200, 86, 255);
+        colorYellow = new Color32(255, 255, 0, 255);
+        colorRed = new Color(255, 0, 0, 255);
     }
 
     // This will be called if the user was hit
     void UpdateHealthBar(float _hitDamage)
     {
-        if (lifeReduce > 0)
+        if (lifeReduce > 0.5)
         {
-            lifeReduce -= _hitDamage;
-            healthBar.localScale = new Vector3(lifeReduce, 1, 1);
-            healthText.text = (Math.Round(lifeReduce, 2) * 100).ToString() + "%";
+            healthText.text = IntToString(_hitDamage);
+            imageColor.color = colorGreen;
         }
+        else if (lifeReduce > 0.2)
+        {
+            healthText.text = IntToString(_hitDamage);
+            imageColor.color = colorYellow;
+        }
+        else if (lifeReduce > 0)
+        {
+            healthText.text = IntToString(_hitDamage);
+            imageColor.color = colorRed;
+        }
+    }
+
+    string IntToString(float _hit)
+    {
+        lifeReduce -= _hit;
+        healthBar.localScale = new Vector3(lifeReduce, 1, 1);
+        return (Math.Round(lifeReduce, 2) * 100).ToString() + "%";
     }
 
     // call this from GameManager
@@ -38,6 +66,7 @@ public class HealthBar : MonoBehaviour
         lifeReduce = 1;
         healthBar.localScale = new Vector3(maxLife, 1, 1);
         healthText.text = originalTextLife;
+        imageColor.color = colorGreen;
     }
 
 }
